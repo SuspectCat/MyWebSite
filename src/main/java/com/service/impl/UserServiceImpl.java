7091c26ebfeb0717d90932fcf4671c275c045dde
夏海 <niyaopasu@gmail.com>
2022-02-02 15:58:10 +0800

    login

--- src/main/java/com/service/impl/UserServiceImpl.java
+++ src/main/java/com/service/impl/UserServiceImpl.java
@@ -5,11 +5,13 @@ import com.mapper.UserMapper;
 import com.pojo.User;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.nio.charset.StandardCharsets;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
